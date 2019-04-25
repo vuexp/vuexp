6e6cc7a2c4cb2d738df674f947675dd671204759
Utku Turkoglu <utkut@comodo.net>
2019-04-25 16:14:02 +0300

    initiate transfer of actionbar and actionitem component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-const placeholder = {};
+// Components
+export { default as ActionBar } from './components/ActionBar';
+export { default as ActionItem } from './components/ActionItem';
+export { default as NavigationButton } from './components/NavigationButton';
 
-export default placeholder;
+// Layouts
+export { default as StackLayout } from './layouts/StackLayout';
