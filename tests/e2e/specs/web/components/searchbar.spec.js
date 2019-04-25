685ee9f13f0ed26248937bb889eee00405eb4a09
volkan akbayir <volkan.akbayir@comodo.com>
2019-04-25 16:52:38 +0300

    transfer searchbar component with improved docs

---
+++ tests/e2e/specs/web/components/searchbar.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tags: ['component', 'label'],
+  'Searchbar Component Test'(browser) {
+    //   browser
+    //     .url('http://localhost:8080/components/search-bar.html')
+    //      TODO : searchbar e2e tests
+    //     .waitForElementVisible('body', 60000)
+    //     .end();
+
+    browser.end();
+  },
+};
