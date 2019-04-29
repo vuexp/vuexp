ae7081d757ee5ecfc668ddf262573e37de2182ba
zhunor <ozerorhun@gmail.com>
2019-04-29 10:50:08 +0300

    Add progress component  - Since button is not merged yet, html button is used

---
+++ docs/.vuepress/components/ProgressDoc.vue
@@ -0,0 +1,50 @@
+<template>
+  <StackLayout class="progress">
+    <Label text="Default Progress"/>
+    <Progress :value="currentProgress" @valueChange="progressChange" :maxValue="maxValueProgress"/>
+    <!-- <Button @tap="incrementValue" text="Increment" class="progress__button" value="Button"/> -->
+  </StackLayout>
+</template>
+
+<script>
+
+import Label from '../../../src/components/Label'
+import StackLayout from '../../../src/layouts/StackLayout'
+import Progress from '../../../src/components/Progress'
+export default {
+  name: 'ProgressDoc',
+  components: {Label, StackLayout,Progress},
+  data() {
+    return {
+      currentProgress: 10,
+      maxValueProgress: 70,
+    };
+  },
+  methods: {
+    progressChange: function(newValue, oldValue) {
+    //   alert('Progress Changed ' + oldValue + ' to ' + newValue);
+    },
+    incrementValue() {
+      if(this.currentProgress === this.maxValueProgress) this.currentProgress = 0;
+      this.currentProgress += 10;
+
+    },
+  },
+  mounted: function() {
+   
+    setInterval(() => {
+      this.incrementValue();
+      console.log('VALUE INCREMENT');
+    }, 1000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.progress {
+  &__button {
+    width: 100px;
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ docs/components/progress.md
@@ -0,0 +1,15 @@
+---
+sidebarDepth: 0
+---
+
+# Progress
+  
+This is an overview of the most common usage of `<Label>`.
+
+<DocExampleBox codeBox="">
+
+```html
+<Label text="Label" />
+```
+<ProgressDoc />
+</DocExampleBox>
