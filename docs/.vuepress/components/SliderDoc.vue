f349fa6443f4526d22234f037ce5ac6b6d533504
Metin Kapı <metin.kapi@nurd.com>
2019-04-29 13:55:53 +0300

    update doc

--- docs/.vuepress/components/SliderDoc.vue
+++ docs/.vuepress/components/SliderDoc.vue
@@ -2,28 +2,36 @@
     <div>
         <Slider :value="value" :min-value="minValue" :max-value="maxValue" @valueChange="onValueChanged($event)" />
         <div>
-            <label>Value: </label>
-            <label class="slider-value">{{value}}</label>
+            <Label text="Value: "/>
+            <Label id="slider__value__label" :text="value"></Label>
         </div>
-            <label>
-                Min Value:
-                <input type="number" v-model="minValue" id="slider__min__value"/>
-            </label>
         <div>
-            <label>Max Value:
-                <input type="number" v-model="maxValue" id="slider__max__value"/>
-            </label>
+            <Label text="Min Value: ">
+                <input type="number" v-model="minValue" id="slider__minValue__input"/>
+            </Label>
+        </div>
+        <div>
+            <Label text="Max Value: ">
+                <input type="number" v-model="maxValue" id="slider__maxValue__input"/>
+            </Label>
+        </div>
+        <div>
+            <Label text="Set Value: ">
+                <input type="number" v-model="value" id="slider__setValue__input"/>
+            </Label>
         </div>
     </div>
 </template>
 
 <script>
     import Slider from '../../../src/components/Slider';
+    import Label from '../../../src/components/Label';
 
     export default {
         name: "SliderDoc",
         components: {
-            Slider
+            Slider,
+            Label
         },
         data() {
             return {
