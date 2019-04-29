4612de9222320878ae4264bd2fea9372b8f67f15
Utku Turkoglu <utkut@comodo.net>
2019-04-29 17:24:53 +0300

    initial transfer

---
+++ docs/.vuepress/components/SwitcherDoc.vue
@@ -0,0 +1,24 @@
+<template>
+	<Switcher v-model="value" @checkedChange="switchValueChange" />
+</template>
+
+<script>
+	import Switcher from '../../../src/components/Switcher'
+
+	export default {
+		name: "SwitcherDoc",
+		components: {
+			Switcher
+		},
+		data() {
+			return {
+				value: true
+			};
+		},
+		methods: {
+			switchValueChange: function (event) {
+				alert(this.value === true ? 'Switcher On' : 'Switcher Off');
+			}
+		}
+	};
+</script>
---
+++ src/components/Switcher.vue
@@ -0,0 +1,31 @@
+<template>
+  <input v-common-directive class="nvw-switch" type="checkbox" :checked="checked" @change="$emit('checkedChange', $event)" @click="updateValue" />
+</template>
+
+<script>
+import CommonDirective from '../directives/CommonDirective';
+import Gestures from '../mixins/GestureMixin';
+
+export default {
+  name: 'Switcher',
+  model: {
+    event: 'input',
+    prop: 'checked',
+  },
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    updateValue: function(event) {
+      this.$emit('input', event.target.checked);
+    },
+  },
+  directives: {
+    'common-directive': CommonDirective,
+  },
+  mixins: [Gestures],
+};
+</script>
