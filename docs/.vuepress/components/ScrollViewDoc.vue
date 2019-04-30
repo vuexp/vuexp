cbef9cd1f04664954022e52b6bf3512091c4613f
alpayk <alpay.kuru@comodo.com>
2019-04-30 16:57:01 +0300

    added QA requirements and ids

--- docs/.vuepress/components/ScrollViewDoc.vue
+++ docs/.vuepress/components/ScrollViewDoc.vue
@@ -1,16 +1,30 @@
 <template>
-<StackLayout class="scroll-view">
-    <ScrollView orientation="horizontal" id="scrollview" class="scroll-view__horizontal">
-      <StackLayout orientation="horizontal">
-        <div>Native Vue Web project is web components for Nativescript-Vue.</div>
-      </StackLayout>
-    </ScrollView>
+<StackLayout class="scroll-view" orientation="horizontal">
+    <StackLayout>
+      <ScrollView :orientation="scrollType" id="scrollview1" :scrollBarIndicatorVisible="scrollBarIndicatorVisible"class="scroll-view__horizontal">
+          <StackLayout orientation="horizontal">
+            <div>Native Vue Web project is web components for Nativescript-Vue.</div>
+          </StackLayout>
+        </ScrollView>
 
-    <ScrollView orientation="vertical" class="scroll-view__vertical">
-      <StackLayout orientation="vertical">
-        <div>Native Vue Web project is web components for Nativescript-Vue. NativeScript is an open source framework for building truly native mobile applications using JavaScript.</div>
-      </StackLayout>
-    </ScrollView>
+        <ScrollView :orientation="scrollType" id="scrollview1" class="scroll-view__vertical" :scrollBarIndicatorVisible="scrollBarIndicatorVisible" >
+          <StackLayout orientation="vertical">
+            <div>Native Vue Web project is web components for Nativescript-Vue. NativeScript is an open source framework for building truly native mobile applications using JavaScript.</div>
+          </StackLayout>
+        </ScrollView>
+    </StackLayout>
+    <StackLayout>
+        <StackLayout orientation="horizontal">
+            <Label text="Scroll Bar Enabled:" id="scrollview_enabled_label" width="200"></Label>
+            <input type="checkbox" v-model="scrollBarIndicatorVisible" id="scrollview_horizontal_input" />
+        </StackLayout>  
+
+
+        <StackLayout orientation="horizontal">
+            <Label text="Horizontal Scrolling:" id="scrollview_orientation_label" width="200"></Label>
+            <input type="checkbox" v-model="horizontal" id="scrollview_horizontal_input" />
+        </StackLayout> 
+    </StackLayout>   
   </StackLayout>
 </template>
 
@@ -18,20 +32,38 @@
 <script>
 import ScrollView from '../../../src/components/ScrollView'
 import StackLayout from '../../../src/layouts/StackLayout'
+import Label from "../../../src/components/Label";
 
 export default {
   name: "ScrollViewDoc",
+  data(){
+    return {
+      scrollBarIndicatorVisible:false,
+      horizontal:false
+    };
+  },
+  computed: {
+    scrollType() {
+      if(this.horizontal) {
+        return "horizontal";
+      }
+      return "vertical";
+    }
+  },
   components :{
     StackLayout,
-    ScrollView
+    ScrollView,
+    Label
   }
 };
+
 </script>
 
 <style lang="scss" scoped>
 .scroll-view {
   &__horizontal {
     width: 150px;
+    height: 50px;
   }
   &__vertical {
     height: 100px;
