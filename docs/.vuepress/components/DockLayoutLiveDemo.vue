6e4a5ad110b04c57c06dfd5c56fcda0115f57f59
Osman Ertem UNAT <osman.unat@nuweb.com>
2019-05-02 15:11:20 +0300

    change html input text usages with TextField

--- docs/.vuepress/components/DockLayoutLiveDemo.vue
+++ docs/.vuepress/components/DockLayoutLiveDemo.vue
@@ -1,42 +1,41 @@
 <template>
   <StackLayout>
-    <StackLayout  class="demo-panel">
+    <StackLayout class="demo-panel">
       <Label text="Add new child" class="demo-panel__title"/>
       <StackLayout orientation="horizontal" style="margin-bottom:5px">
         <StackLayout orientation="horizontal" class="editor-param">
-        <Label text="Stretch Last child: "/>
-        <input
-          id="docklayout_stretchlastchild_cb"
-          type="checkbox"
-          v-model="stretchLastChildSelected"
-        >
-        </StackLayout>
-        <StackLayout orientation="horizontal" class="editor-param">
-          <Label class="editor-param-title" text="Text: "/>
+          <Label text="Stretch Last child: "/>
           <input
-            id="docklayout_childname_input"
-            class="editor-param-input"
-            type="text"
-            v-model="dockChildText"
+            id="docklayout_stretchlastchild_cb"
+            type="checkbox"
+            v-model="stretchLastChildSelected"
           >
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
+          <Label class="editor-param-title" text="Text: "/>
+          <TextField
+            v-model="dockChildText"
+            id="docklayout_childname_input"
+            class="editor-param-input"
+          />
+        </StackLayout>
+        <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="Width: "/>
-          <input
+          <TextField
             id="docklayout_childtwidth_input"
             class="editor-param-input"
             type="text"
             v-model="dockChildWidth"
-          >
+          />
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="Height: "/>
-          <input
+          <TextField
             id="docklayout_childheight_input"
             class="editor-param-input"
             type="text"
             v-model="dockChildHeight"
-          >
+          />
         </StackLayout>
       </StackLayout>
 
@@ -78,16 +77,21 @@
 
       <StackLayout orientation="horizontal" class="editor-param">
         <StackLayout class="editor-param">
-          <button id="docklayout_addchild_button" @click="addChildNode">➕Add Child</button>
+          <Button id="docklayout_addchild_button" text="➕ Add Child" @tap="addChildNode"/>
         </StackLayout>
         <StackLayout class="editor-param">
-          <button
+          <Button
             id="docklayout_removeallchildren_button"
-            @click="clearDockChildList"
-          >❌Remove All Children</button>
+            @tap="addChildNode"
+            text="❌ Remove All Children"
+          />
         </StackLayout>
         <StackLayout class="editor-param">
-          <button id="docklayoutdoc-checkbox-textwrap" @click="renderSampleSet">Render Sample Set</button>
+          <Button
+            id="docklayoutdoc_rendersample_button"
+            @click="renderSampleSet"
+            text=" 🔷 Render Sample Set"
+          />
         </StackLayout>
       </StackLayout>
     </StackLayout>
@@ -98,25 +102,28 @@
       <StackLayout orientation="horizontal" v-for="(dockChild, index) in dockChildrenList">
         <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="T: "/>
-          <input :id="'docklayout_childreneditor_name' + index" type="text" style="width:50px" v-model="dockChildrenList[index].text">
+          <TextField
+            :id="'docklayout_childreneditor_name' + index"
+            type="text"
+            style="width:50px"
+            v-model="dockChildrenList[index].text"
+          />
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="W:"/>
-          <input
+          <TextField
             :id="'docklayout_childreneditor_width' + index"
             class="editor-param-input"
-            type="text"
             v-model="dockChildrenList[index].width"
-          >
+          />
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="H: "/>
-          <input
+          <TextField
             :id="'docklayout_childreneditor_height' + index"
             class="editor-param-input"
-            type="text"
             v-model="dockChildrenList[index].height"
-          >
+          />
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
           <Label class="editor-param-title" text="C: "/>
@@ -151,7 +158,12 @@
           </select>
         </StackLayout>
         <StackLayout orientation="horizontal" class="editor-param">
-          <button :id="'docklayout_childreneditor_removechild' + index" @click="removeChildAtIndex(index)">❌</button>
+          <Button
+            :id="'docklayout_childreneditor_removechild' + index"
+            @tap="removeChildAtIndex(index)"
+            text="❌"
+            style="font-size:9px"
+          />
         </StackLayout>
       </StackLayout>
     </StackLayout>
@@ -159,7 +171,11 @@
 
     <StackLayout style="margin-top:20px" class="demo-panel">
       <Label text="Rendered Result" class="demo-panel__title"/>
-      <DockLayout id="docklayout_thedocklayout" :stretchLastChild="stretchLastChildSelected" class="dock">
+      <DockLayout
+        id="docklayout_thedocklayout"
+        :stretchLastChild="stretchLastChildSelected"
+        class="dock"
+      >
         <Label
           v-for="(dockChild, index) in dockChildrenList"
           v-bind="dockChild"
@@ -172,7 +188,9 @@
 
 <script>
 import Label from '../../../src/components/Label';
+import Button from '../../../src/components/Button';
 import DockLayout from '../../../src/layouts/DockLayout';
+import TextField from '../../../src/components/TextField';
 import StackLayout from '../../../src/layouts/StackLayout';
 
 export default {
@@ -206,10 +224,10 @@ export default {
     },
     renderSampleSet() {
       this.dockChildrenList = [];
-      this.dockChildrenList.push({ text: 'left', dock: 'left', width: 41, height: '', backgroundColor: 'gray' });
-      this.dockChildrenList.push({ text: 'top', dock: 'top', width: '', height: 42, backgroundColor: 'red' });
-      this.dockChildrenList.push({ text: 'right', dock: 'right', width: 43, height: '', backgroundColor: 'yellow' });
-      this.dockChildrenList.push({ text: 'bottom', dock: 'bottom', width: '', height: 44, backgroundColor: 'blue' });
+      this.dockChildrenList.push({ text: 'left', dock: 'left', width: '41', height: '', backgroundColor: 'gray' });
+      this.dockChildrenList.push({ text: 'top', dock: 'top', width: '', height: '42', backgroundColor: 'red' });
+      this.dockChildrenList.push({ text: 'right', dock: 'right', width: '43', height: '', backgroundColor: 'yellow' });
+      this.dockChildrenList.push({ text: 'bottom', dock: 'bottom', width: '', height: '44', backgroundColor: 'blue' });
       this.dockChildrenList.push({ text: 'center', dock: 'center', backgroundColor: 'cyan' });
     },
     removeChildAtIndex(index) {
@@ -223,6 +241,8 @@ export default {
     Label,
     DockLayout,
     StackLayout,
+    Button,
+    TextField,
   },
 };
 </script>
@@ -246,8 +266,11 @@ export default {
   margin-left: 5px;
 }
 
-button {
-  height: 22px;
+.vxp-button {
+  padding: unset;
+  padding-left: 5px;
+  padding-right: 5px;
+  min-width: unset;
 }
 
 .demo-panel {
@@ -255,12 +278,12 @@ button {
   background-color: lightgray;
   border: #3c495e;
   border-radius: 5px;
-  margin:10px;
+  margin: 10px;
 
   &__title {
-    font-size:20px; 
-    font-weight:bold; 
-    margin-bottom:10px;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 10px;
   }
 }
 </style>
