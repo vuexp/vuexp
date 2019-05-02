b8c1594dd9c695f6cf0fcfc9d2aa77346c7849b8
alpayk <alpay.kuru@comodo.com>
2019-05-02 18:10:27 +0300

    update button doc

--- docs/.vuepress/components/ButtonDoc.vue
+++ docs/.vuepress/components/ButtonDoc.vue
@@ -1,19 +1,17 @@
 <template>
   <StackLayout>
     <Button id="button" :text="buttonText" @tap="onButtonTap" :textWrap="textWrap" horizontalAlignment="left" />
-    <Button id="button"  @tap="onButtonTap" :textWrap="textWrap" horizontalAlignment="left"> Button Text </Button>
-
     <StackLayout orientation="horizontal">
-      <Label text="Text:" width="100"></Label>
-      <input type="text" id="button_text_input" v-model="buttonText" />
+      <Label text="Text:" width="100" class="scroll-view__margin_top"></Label>
+      <input type="text" id="button_text_input" v-model="buttonText" class="scroll-view__margin_top" />
     </StackLayout>  
     <StackLayout orientation="horizontal">
-      <Label text="Label:" width="100"></Label>
-      <Label :text="labelText" id="button_label" ></Label>
+      <Label text="Label:" width="100" class="scroll-view__margin_top"></Label>
+      <Label :text="labelText" id="button_label" class="scroll-view__margin_top" ></Label>
     </StackLayout>
     <StackLayout orientation="horizontal">
-      <Label text="Text Wrap :" width="100"></Label>
-      <input type="checkbox" v-model="textWrap" id="button_textWrap_input" />
+      <Label text="Text Wrap :" width="100" class="scroll-view__margin_top"></Label>
+      <input type="checkbox" v-model="textWrap" id="button_textWrap_input" class="scroll-view__margin_top" />
     </StackLayout>
   </StackLayout>
 </template>
@@ -44,3 +42,10 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+.scroll-view {
+   &__margin_top {
+   margin-top: 10px;
+  }
+}
+</style>
