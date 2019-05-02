77c46d0c630ebd3202a8db0e24ff3364edef053a
zhunor <ozerorhun@gmail.com>
2019-05-02 16:27:59 +0300

    Auto-fix by lint

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 export { default as SegmentedBar } from './components/SegmentedBar';
 export { default as SegmentedBarItem } from './components/SegmentedBarItem';
 export { default as ModalDialog } from './components/ModalDialog';
-
