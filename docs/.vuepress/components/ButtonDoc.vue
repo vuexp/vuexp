c1cd89f774c7164c14bd2719b89252407922f13f
alpayk <alpay.kuru@comodo.com>
2019-05-03 10:31:03 +0300

    added width and height in live demo

--- docs/.vuepress/components/ButtonDoc.vue
+++ docs/.vuepress/components/ButtonDoc.vue
@@ -1,6 +1,6 @@
 <template>
   <StackLayout>
-    <Button id="button" :text="buttonText" @tap="onButtonTap" :textWrap="textWrap" horizontalAlignment="left" />
+    <Button id="button" :text="buttonText" @tap="onButtonTap" :style="{'width': width + 'px' ,'height': height + 'px' }" :textWrap="textWrap" horizontalAlignment="left" />
     <StackLayout orientation="horizontal">
       <Label text="Text:" width="100" class="scroll-view__margin_top"></Label>
       <input type="text" id="button_text_input" v-model="buttonText" class="scroll-view__margin_top" />
@@ -13,6 +13,14 @@
       <Label text="Text Wrap :" width="100" class="scroll-view__margin_top"></Label>
       <input type="checkbox" v-model="textWrap" id="button_textWrap_input" class="scroll-view__margin_top" />
     </StackLayout>
+     <StackLayout orientation="horizontal">
+      <Label text="Width :" width="100" class="scroll-view__margin_top"></Label>
+      <TextField type="number" v-model="width" id="button_width_input" class="scroll-view__margin_top" />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <Label text="Height :" width="100" class="scroll-view__margin_top"></Label>
+      <TextField type="number" v-model="height" id="button_height_input" class="scroll-view__margin_top" />
+    </StackLayout>
   </StackLayout>
 </template>
 
@@ -20,6 +28,7 @@
 import Button from "../../../src/components/Button";
 import Label from "../../../src/components/Label";
 import StackLayout from "../../../src/layouts/StackLayout";
+import TextField from "../../../src/components/TextField";
 
 export default {
   name: "ButtonDoc",
@@ -27,7 +36,9 @@ export default {
       return {
           labelText : '',
           textWrap : false,
-          buttonText: 'Button'
+          buttonText: 'Button',
+          width :100,
+          height :40
       };
   },
   methods: {
@@ -38,7 +49,8 @@ export default {
   components:{
      Button,
      Label,
-     StackLayout
+     StackLayout,
+     TextField
   }
 };
 </script>
