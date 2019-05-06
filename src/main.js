0edf96e0a45c5e486b7b9399b64520552bddb544
Utku Turkoglu <utkut@comodo.net>
2019-05-06 15:44:41 +0300

    removed absolute from main

--- src/main.js
+++ src/main.js
@@ -4,8 +4,5 @@ export { default as ActionItem } from './components/ActionItem';
 export { default as NavigationButton } from './components/NavigationButton';
 export { default as Label } from './components/Label';
 
-// Layouts
-export { default as AbsoluteLayout } from './layouts/AbsoluteLayout';
-
 // Dialogs
 export { default as ModalDialog } from './components/ModalDialog';
