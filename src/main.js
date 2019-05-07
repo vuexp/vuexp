beb6446437ad7fedbdef0632a96feb9ee8fafbee
alpayk <alpay.kuru@comodo.com>
2019-05-07 14:43:57 +0300

    deleted label from main js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const placeholder = {};
 
 export default placeholder;
-export { default as Label } from './components/Label';
 
 export { default as ScrollView } from './components/ScrollView';
 // Components
