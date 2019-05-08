460a8122332d14f6a3e322772f4a8aef26cc98af
Utku Turkoglu <utkut@comodo.net>
2019-05-08 11:00:56 +0300

    changed style, added css background and color inputs

--- docs/.vuepress/components/SwitcherDoc.vue
+++ docs/.vuepress/components/SwitcherDoc.vue
@@ -1,24 +1,35 @@
 <template>
-	<Switcher v-model="value" @checkedChange="switchValueChange" />
+  <StackLayout>
+    <Switcher v-model="value" @checkedChange="switchValueChange"/>
+    <Label id="switcher__value__label" style="margin-top: 20px;" :text="labelText"></Label>
+  </StackLayout>
 </template>
 
 <script>
-	import Switcher from '../../../src/components/Switcher'
+import Switcher from '../../../src/components/Switcher';
+import Label from '../../../src/components/Label';
+import StackLayout from '../../../src/layouts/StackLayout';
 
-	export default {
-		name: "SwitcherDoc",
-		components: {
-			Switcher
-		},
-		data() {
-			return {
-				value: true
-			};
-		},
-		methods: {
-			switchValueChange: function (event) {
-				alert(this.value === true ? 'Switcher On' : 'Switcher Off');
-			}
-		}
-	};
+export default {
+  name: 'SwitcherDoc',
+  components: {
+    Switcher,
+    StackLayout,
+    Label,
+  },
+  data() {
+    return {
+      value: true,
+      labelText: 'Switcher On',
+    };
+  },
+  methods: {
+    switchValueChange: function(event) {
+      this.labelText = this.value === true ? 'Switcher On' : 'Switcher Off';
+    },
+  },
+};
 </script>
+<style lang="scss">
+</style>
+
