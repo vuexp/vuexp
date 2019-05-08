d726289b68e76d67856b9062df37515cbe09491b
serhat kemal <serhat.buyukcelebi@comodo.com>
2019-05-08 10:21:10 +0300

    Fixed lint issues.

--- docs/.vuepress/components/AlertDialogDoc.vue
+++ docs/.vuepress/components/AlertDialogDoc.vue
@@ -1,42 +1,40 @@
 <template>
-    <StackLayout>
-        <Button text="Show Alert Dialog" width="200" @tap="showAlertDialog" id="alertDialog_docs" />
-        <FlexboxLayout flexDirection="row" flexWrap="wrap">
-            <Label text="Clicked Action:" class="label"></Label>
-            <Label :text="eventText" class="form-input" id="alertDialog_event_label"></Label>
-        </FlexboxLayout>
-    </StackLayout>
+  <StackLayout>
+    <Button text="Show Alert Dialog" width="200" @tap="showAlertDialog" id="alertDialog_docs" />
+    <FlexboxLayout flexDirection="row" flexWrap="wrap">
+      <Label text="Clicked Action:" class="label"></Label>
+      <Label :text="eventText" class="form-input" id="alertDialog_event_label"></Label>
+    </FlexboxLayout>
+  </StackLayout>
 </template>
 
 <script>
-    import Label from '../../../src/components/Label';
-    import StackLayout from '../../../src/layouts/StackLayout';
-    import Button from '../../../src/components/Button';
-    import FlexboxLayout from "../../../src/layouts/FlexboxLayout";
+import Label from '../../../src/components/Label';
+import StackLayout from '../../../src/layouts/StackLayout';
+import Button from '../../../src/components/Button';
+import FlexboxLayout from '../../../src/layouts/FlexboxLayout';
 
-    export default {
-        name: "AlertDialogDoc",
-        data() {
-            return {
-                eventText: '-'
-            };
-        },
-        methods: {
-            showAlertDialog: function() {
-                alert({title: "Your title", message: "Your message", okButtonText: "OK"}).then(() => {
-                    this.eventText = 'OK';
-                });
-            }
-        },
-        components: {
-            FlexboxLayout,
-            StackLayout,
-            Label,
-            Button
-        }
+export default {
+  name: 'AlertDialogDoc',
+  data() {
+    return {
+      eventText: '-',
     };
+  },
+  methods: {
+    showAlertDialog: function() {
+      alert({ title: 'Your title', message: 'Your message', okButtonText: 'OK' }).then(() => {
+        this.eventText = 'OK';
+      });
+    },
+  },
+  components: {
+    FlexboxLayout,
+    StackLayout,
+    Label,
+    Button,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
-
+<style lang="scss" scoped></style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,20 +1,20 @@
-import * as Plugins from '../../src/plugins'
+import * as Plugins from '../../src/plugins';
 export default ({
-                    Vue, // the version of Vue being used in the VuePress app
-                    options, // the options for the root Vue instance
-                    router // the router instance for the app
-                }) => {
-    if (typeof document !== "undefined" && typeof window !== "undefined") {
-        Object.keys(Plugins).forEach(function(key) {
-            if (Plugins[key].name) {
-                // Component registration
-                Vue.component(key, Plugins[key]);
-            } else if (Plugins[key].install) {
-                // Plugin registration
-                Vue.use(Plugins[key]);
-            }
-        });
-    }
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+}) => {
+  if (typeof document !== 'undefined' && typeof window !== 'undefined') {
+    Object.keys(Plugins).forEach(function(key) {
+      if (Plugins[key].name) {
+        // Component registration
+        Vue.component(key, Plugins[key]);
+      } else if (Plugins[key].install) {
+        // Plugin registration
+        Vue.use(Plugins[key]);
+      }
+    });
+  }
 
-    router.push("/docs/");
+  router.push('/docs/');
 };
