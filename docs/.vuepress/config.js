7fa54072647859a71a8c3f66bd2799a2b3533e7e
gucluk <guclu.kivanc@nurd.com>
2019-05-08 12:25:16 +0300

    Fixed conflict on config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,7 +40,7 @@ module.exports = {
             "components/textfield.md",
             "components/slider.md",
             "components/textview.md",
-              "components/frame.md",
+            "components/frame.md",
           ]
         },
       ]
