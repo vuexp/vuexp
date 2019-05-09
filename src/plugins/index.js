799c5b07d642a15aab554828923999df963c0259
alpayk <alpay.kuru@comodo.com>
2019-05-09 17:02:01 +0300

    code beat fix

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -10,23 +10,27 @@ const NvwPlugin = {
       const confirmDialogDom = confirmDialog.$mount().$el;
       document.body.appendChild(confirmDialogDom);
 
-      return new Promise(resolve => {
-        if (typeof messageText === 'string') {
-          confirmDialog.message = messageText;
-        } else {
-          const { title, message, okButtonText, cancelButtonText } = messageText;
-          confirmDialog.title = title;
-          confirmDialog.message = message;
-          confirmDialog.okButtonText = okButtonText;
-          confirmDialog.cancelButtonText = cancelButtonText;
-        }
-        confirmDialog.isModalVisible = true;
-        confirmDialog.$once('submit', val => {
-          confirmDialog.isModalVisible = false;
-          resolve(val);
-        });
-      });
+      return confirmWrapper(messageText, confirmDialog);
     };
   },
 };
+
 export default NvwPlugin;
+function confirmWrapper(messageText, confirmDialog) {
+  return new Promise(resolve => {
+    if (typeof messageText === 'string') {
+      confirmDialog.message = messageText;
+    } else {
+      const { title, message, okButtonText, cancelButtonText } = messageText;
+      confirmDialog.title = title;
+      confirmDialog.message = message;
+      confirmDialog.okButtonText = okButtonText;
+      confirmDialog.cancelButtonText = cancelButtonText;
+    }
+    confirmDialog.isModalVisible = true;
+    confirmDialog.$once('submit', val => {
+      confirmDialog.isModalVisible = false;
+      resolve(val);
+    });
+  });
+}
