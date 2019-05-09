26b75f383d4b32f19e99b91f9f41e3a20dd5af67
alpayk <alpay.kuru@comodo.com>
2019-05-09 14:10:32 +0300

    added label

--- docs/.vuepress/components/ScrollViewDoc.vue
+++ docs/.vuepress/components/ScrollViewDoc.vue
@@ -1,19 +1,26 @@
 <template>
 <StackLayout class="scroll-view" orientation="horizontal">
-        <ScrollView :orientation="scrollType" id="scrollview_container" class="scroll-view__vertical" :scrollBarIndicatorVisible="scrollBarIndicatorVisible" >
-          <StackLayout orientation="vertical">
-            <div>Vuexp project is web components for Nativescript-Vue. NativeScript is an open source framework for building truly native mobile applications using JavaScript.</div>
-          </StackLayout>
-        </ScrollView>
-       <StackLayout orientation="horizontal">
+    <ScrollView :orientation="scrollType" id="scrollview_container" @scroll="onScroll" class="scroll-view__vertical" :scrollBarIndicatorVisible="scrollBarIndicatorVisible" >
+      <StackLayout orientation="vertical">
+        <div>Vuexp project is web components for Nativescript-Vue. NativeScript is an open source framework for building truly native mobile applications using JavaScript.</div>
+      </StackLayout>
+    </ScrollView>
+   <StackLayout orientation="vertical">
+     <StackLayout orientation="horizontal">
+        <StackLayout orientation="horizontal">
             <Label text="Scroll Bar Enabled:" id="scrollview_enabled_label" class="scroll-view__margin_left" width="200"></Label>
             <input aria-label="Scroll Bar Enabled" type="checkbox" v-model="scrollBarIndicatorVisible" id="scrollview_horizontal_input" />
         </StackLayout>  
-     <StackLayout orientation="horizontal">
+        <StackLayout orientation="horizontal">
             <Label text="Horizontal Scrolling:" id="scrollview_orientation_label" class="scroll-view__margin_left" width="200"></Label>
             <input aria-label="Horizontal Scrolling" type="checkbox" v-model="horizontal" id="scrollview_horizontal_input" />
         </StackLayout> 
-      
+     </StackLayout> 
+      <StackLayout orientation="horizontal">
+          <Label :text="scrollText" id="scrollview_scroll_label" class="scroll-view__margin_left"></Label>          
+         
+      </StackLayout>       
+    </StackLayout>
   </StackLayout>
 </template>
 
@@ -22,14 +29,24 @@
 import ScrollView from '../../../src/components/ScrollView'
 import StackLayout from '../../../src/layouts/StackLayout'
 import Label from "../../../src/components/Label";
+import Button from "../../../src/components/Button";
 
 export default {
   name: "ScrollViewDoc",
   data(){
     return {
       scrollBarIndicatorVisible:false,
-      horizontal:false
+      horizontal:false,
+      scrollText: ''
     };
+  },
+  methods: {
+    onScroll(){
+      this.scrollText = 'scrolled';
+    },
+    clearScrollText() {
+      this.scrollText = '';
+    }
   },
   computed: {
     scrollType() {
@@ -42,7 +59,8 @@ export default {
   components :{
     StackLayout,
     ScrollView,
-    Label
+    Label,
+    Button
   }
 };
 
