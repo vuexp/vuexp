d544f6be267ccaddcf238de393ec7e76798e8a41
iremo <irem.ozdemir2609@gmail.com>
2019-05-16 15:02:10 +0300

    switch-e2e-iremdamla

--- tests/e2e/specs/web/functionality/components/switch/checked-true.spec.js
+++ tests/e2e/specs/web/functionality/components/switch/checked-true.spec.js
@@ -1,9 +1,9 @@
 module.exports = {
-  tags: ['component', 'textfield'],
+  tags: ['component', 'switch'],
   before: function(client, done) {
     this.currentPage = client.maximizeWindow().page.switchPage();
     this.currentPage
-      .navigate('http://localhost:8080/components/switcher.html')
+      .navigate(client.globals.devUrl + 'components/switcher.html')
       .waitForElementVisible('body', 60000)
       .customPerform(function() {
         done();
