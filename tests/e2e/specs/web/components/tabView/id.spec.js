b86cd0700dfc5f0711537dee592d198b9f8c0f21
COMODO\aylino <aylin.ozguven@comodo.com>
2019-05-16 18:03:09 +0300

    update navigate url

--- tests/e2e/specs/web/components/tabView/id.spec.js
+++ tests/e2e/specs/web/components/tabView/id.spec.js
@@ -3,7 +3,7 @@ module.exports = {
   before: function(client, done) {
     this.currentPage = client.maximizeWindow().page.tabViewPage();
     this.currentPage
-      .navigate('http://localhost:8080/components/tabview.html')
+      .navigate(client.globals.devUrl + 'components/tabview.html')
       .waitForElementVisible('body', 60000)
       .customPerform(function() {
         done();
