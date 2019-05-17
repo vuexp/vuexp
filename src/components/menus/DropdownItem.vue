e30d6cc7b648bbea7e73f3ac021748c6f9fe516b
Utku Turkoglu <utkut@comodo.net>
2019-05-17 14:35:36 +0300

    init menu and item

---
+++ src/components/menus/DropdownItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <StackLayout class="vxp-dropdown-item" orientation="horizontal" @tap="itemClicked($event)">
+    <Label v-if="(icon && iconClass) || (item.icon && item.iconClass)" :text="(icon || item.icon) | fonticon" :class="iconClass || item.iconClass"></Label>
+    <Label v-if="item.title" :text="item.title" class="vxp-dropdown-item__title"></Label>
+    <Label v-else :text="item" class="vxp-dropdown-item__title"></Label>
+  </StackLayout>
+</template>
+
+<script>
+import Label from '../Label';
+import StackLayout from '../../layouts/StackLayout';
+
+export default {
+  name: 'DropdownItem',
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  props: {
+    item: {
+      type: Object,
+      default: null,
+    },
+    title: String,
+    icon: String,
+    iconClass: String,
+  },
+  methods: {
+    itemClicked() {
+      this.$emit('submit', this.cancelButtonText);
+    },
+  },
+  components: {
+    StackLayout,
+    Label,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/components/dialogs';
+
+.vxp-action-dialog {
+  &__body {
+    &__item {
+      padding: 0;
+      margin: 0;
+      .vxp-button {
+        width: 100%;
+        text-align: left;
+        background-color: #fff;
+        border: none;
+        outline: none;
+        padding: 8px 6px;
+        margin: 2px 0;
+
+        &:hover {
+          cursor: pointer;
+          background-color: #ebebeb;
+        }
+      }
+    }
+  }
+  &__footer {
+    &__cancel-button {
+      @include vxp-dialog-button;
+    }
+  }
+}
+</style>
---
+++ src/components/menus/DropdownMenu.vue
@@ -0,0 +1,68 @@
+<template>
+  <StackLayout class="vxp-dropdown">
+    <StackLayout :disabled="disabled" v-if="items">
+      <DropdownItem class="vxp-dropdown__item" v-for="(item, key) in items" :item="item" :key="key" />
+    </StackLayout>
+    <slot></slot>
+  </StackLayout>
+</template>
+
+<script>
+import DropdownItem from './DropdownItem';
+import StackLayout from '../../layouts/StackLayout';
+
+export default {
+  name: 'DropdownMenu',
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    itemClicked() {
+      this.$emit('submit', this.cancelButtonText);
+    },
+  },
+  components: {
+    StackLayout,
+    DropdownItem,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/components/dialogs';
+
+.vxp-action-dialog {
+  &__body {
+    &__item {
+      padding: 0;
+      margin: 0;
+      .vxp-button {
+        width: 100%;
+        text-align: left;
+        background-color: #fff;
+        border: none;
+        outline: none;
+        padding: 8px 6px;
+        margin: 2px 0;
+
+        &:hover {
+          cursor: pointer;
+          background-color: #ebebeb;
+        }
+      }
+    }
+  }
+  &__footer {
+    &__cancel-button {
+      @include vxp-dialog-button;
+    }
+  }
+}
+</style>
