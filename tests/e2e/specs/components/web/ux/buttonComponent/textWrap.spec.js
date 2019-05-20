bc19807761b071b22b25781f2875f70855c62de3
mchtctn <cetin.mcht@gmail.com>
2019-05-20 10:44:43 +0300

    update button component test

--- tests/e2e/specs/components/web/ux/buttonComponent/textWrap.spec.js
+++ tests/e2e/specs/components/web/ux/buttonComponent/textWrap.spec.js
@@ -27,6 +27,23 @@ module.exports = {
       .assert.containsText('@buttonActionLabel', 'Button Pressed');
   },
 
+  'C13843420 Props/Name:textWrap, Value: True, Events/tap'() {
+    this.currentPage
+      .setTextToButton(
+        'All I want is a proper cup of coffee,' +
+          'Made in a proper copper coffee pot' +
+          'I may be off my dot' +
+          'But I want a cup of coffee' +
+          'From a proper coffee pot.',
+      )
+      .setButtonWidth(300)
+      .setButtonHeight(120)
+      .checkTextWrapSelectBox(300, 120)
+      .checkScreenshot('@buttonComponent')
+      .click('@buttonComponent')
+      .assert.containsText('@buttonActionLabel', 'Button Pressed');
+  },
+
   after: function(client, done) {
     client.end().customPerform(done);
   },
