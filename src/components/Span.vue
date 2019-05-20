a79eac674bd4f44ec037186b8956dc0db89d219a
mylmz10 <yilmaz.muammer@outlook.com>
2019-05-20 09:37:44 +0300

    FormattedString and Span components transfer

---
+++ src/components/Span.vue
@@ -0,0 +1,22 @@
+<template>
+  <span v-common-directive class="nvw-span">
+    {{ text }}
+    <slot />
+  </span>
+</template>
+
+<script>
+import CommonDirective from '../directives/CommonDirective';
+import Gestures from '../mixins/GestureMixin';
+
+export default {
+  name: 'Span',
+  props: {
+    text: String,
+  },
+  directives: {
+    'common-directive': CommonDirective,
+  },
+  mixins: [Gestures],
+};
+</script>
