18a440f0a34a18a6904d44c2fa4c606fd1e1cf11
Metin Kapı <metin.kapi94@gmail.com>
2019-05-21 15:36:51 +0300

    Rename image component

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,7 +47,7 @@ module.exports = {
             "components/frame.md",
             "components/page.md",
             "components/tabview.md",
-            "components/image.md",
+            "components/vxpimage.md",
             "components/segmented-bar.md",
             "components/progress.md",
             "components/vxpcheckbox.md",
