8a8ac58699552049f3bd2d8b9452ef4342361a95
Osman Ertem UNAT <osmanertem@gmail.com>
2019-05-21 18:06:01 +0300

    update unit test

--- tests/unit/components/VxpLabel.spec.js
+++ tests/unit/components/VxpLabel.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { mount } from '@vue/test-utils';
 import VxpLabel from '../../../src/components/VxpLabel.vue';
-import Label from '../../../src/components/Label.vue';
+import Label from '../../../src/core/components/Label/Label';
 
 describe('VxpLabel.vue', () => {
   const wrapper = mount(VxpLabel);
