fa5af12c1985bfbaaf83808bce0adb1264bc1796
Utku Turkoglu <utkut@comodo.net>
2019-05-21 13:28:38 +0300

    removed style comment

--- src/components/menus/DropdownMenu.vue
+++ src/components/menus/DropdownMenu.vue
@@ -100,7 +100,6 @@ export default {
     position: relative;
     top: 0.2rem;
     background: white;
-    // box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
     border-radius: 7px;
     padding: 0.5rem;
