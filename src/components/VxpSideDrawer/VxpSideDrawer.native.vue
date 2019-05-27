2d9fa6edffac5aa9f0fadea2c3c31b4db58289ba
iremo <irem.ozdemir2609@gmail.com>
2019-05-27 15:04:33 +0300

    slider-fix-textfield

---
+++ src/components/VxpSideDrawer/VxpSideDrawer.native.vue
@@ -0,0 +1,30 @@
+<template>
+  <RadSideDrawer ref="drawer" drawerLocation="Left" drawerContentSize="auto">
+    <StackLayout v-view:drawerContent>
+      <slot name="drawerContent"></slot>
+    </StackLayout>
+    <StackLayout v-view:mainContent>
+      <slot name="mainContent"></slot>
+    </StackLayout>
+  </RadSideDrawer>
+</template>
+
+<script>
+import Vue from 'nativescript-vue'; // eslint-disable-line
+import SideDrawerPlugin from 'nativescript-ui-sidedrawer/vue';
+
+Vue.use(SideDrawerPlugin);
+export default {
+  name: 'VxpSideDrawer',
+  mounted() {},
+  methods: {
+    showDrawer() {
+      this.$refs.drawer.showDrawer();
+    },
+    closeDrawer() {
+      this.$refs.drawer.closeDrawer();
+    },
+  },
+};
+</script>
+<style lang="sass" scoped></style>
