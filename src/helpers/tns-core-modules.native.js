ef050d85edbabc0a71fa5ee71c36a3a594c8cca1
Ercan Gercek <ercangercek@gmail.com>
2019-05-28 17:23:40 +0300

    fixed lint problem

--- src/helpers/tns-core-modules.native.js
+++ src/helpers/tns-core-modules.native.js
@@ -7,6 +7,6 @@ const imageSourceModule = require('tns-core-modules/image-source'); //eslint-dis
 const fileSystemModule = require('tns-core-modules/file-system'); //eslint-disable-line
 const imageModule = require('tns-core-modules/ui/image'); //eslint-disable-line
 
-export { FormattedString } from 'tns-core-modules/text/formatted-string';
-export { Span } from 'tns-core-modules/text/span';
+export { FormattedString } from 'tns-core-modules/text/formatted-string'; //eslint-disable-line
+export { Span } from 'tns-core-modules/text/span'; //eslint-disable-line
 export { utilsModule, frameModule, imageSourceModule, fileSystemModule, GC, ImageAsset, imageModule, application };
