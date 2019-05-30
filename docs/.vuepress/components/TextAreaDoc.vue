a77225c2470ab9b1ad7efc97e26f8f1fbdc9eef4
gucluk <guclu.kivanc@nurd.com>
2019-05-30 15:19:36 +0300

    TextArea Ready to Test

--- docs/.vuepress/components/TextAreaDoc.vue
+++ docs/.vuepress/components/TextAreaDoc.vue
@@ -1,8 +1,9 @@
 <template>
-	<StackLayout class="textView-wrapper">
-		<StackLayout orientation="horizontal">
-			<Label text="TextArea:" />
-			<TextArea id="TextView"
+  <StackLayout class="textView-wrapper">
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="TextArea:" />
+      <TextArea
+        id="TextArea"
         v-model="text"
         :hint="hint"
         :editable="editable"
@@ -12,81 +13,86 @@
         @textChange="eventOutput = 'textChange'"
         @returnPress="eventOutput = 'returnPress'"
         @focus="eventOutput = 'focus'"
-        @blur="eventOutput = 'blur'" />
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="text:" horizontalAlignment="50%" />
-			<TextField v-model="text" id="TextView_text_textfield" horizontalAlignment="50%" />
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="hint:" />
-			<TextField v-model="hint" id="TextView_hint_textfield" />
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="editable:" />
-			<input type="checkbox" v-model="editable" id="TextView_editable_input">
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="maxLength:" />
-			<TextField v-model="maxLength" keyboardType="number" id="TextView_maxLength_textfield" />
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="returnKeyType:" />
-			<select v-model="returnKeyType" id="TextView_returnKeyType_select">
-				<option value="done" id="TextView_returnKeyType_done_option">done</option>
-				<option value="next" id="TextView_returnKeyType_next_option">next</option>
-				<option value="go" id="TextView_returnKeyType_go_option">go</option>
-				<option value="search" id="TextView_returnKeyType_search_option">search</option>
-				<option value="send" id="TextView_returnKeyType_send_option">send</option>
-			</select>
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="autocorrect:" />
-			<input type="checkbox" v-model="autocorrect" id="TextView_autocorrect_input">
-		</StackLayout>
-		<StackLayout orientation="horizontal">
-			<Label text="Event output:" />
-			<Label :text="eventOutput" id="TextView_eventOutput_Label" />
-		</StackLayout>
-	</StackLayout>
+        @blur="eventOutput = 'blur'"
+      />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="text:" horizontalAlignment="50%" />
+      <TextField v-model="text" id="textArea__text__input" horizontalAlignment="50%" />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="hint (placeholder):" />
+      <TextField v-model="hint" id="textArea__hint__input" />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="editable (disabled):" />
+      <VxpCheckbox id="textArea__editable__input" :checked="editable" @change="editable = !editable"/>
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="maxLength:" />
+      <TextField v-model="maxLength" keyboardType="number" id="textArea___maxlength__input" />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="returnKeyType:" />
+      <select v-model="returnKeyType" id="textArea__returnKeyType__select">
+        <option value="done" id="TextView_returnKeyType_done_option">done</option>
+        <option value="next" id="TextView_returnKeyType_next_option">next</option>
+        <option value="go" id="TextView_returnKeyType_go_option">go</option>
+        <option value="search" id="TextView_returnKeyType_search_option">search</option>
+        <option value="send" id="TextView_returnKeyType_send_option">send</option>
+      </select>
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="autocorrect:" />
+      <VxpCheckbox v-model="autocorrect" id="textArea__autocorrect__input" />
+    </StackLayout>
+    <StackLayout orientation="horizontal">
+      <VxpLabel text="Event output:" />
+      <VxpLabel :text="eventOutput" id="textArea__eventOutput__label" />
+    </StackLayout>
+  </StackLayout>
 </template>
 
 <script>
-  import TextArea from '../../../src/core/components/TextArea/TextArea'
-  import TextField from '../../../src/core/components/TextField/TextField'
-	import Label from '../../../src/core/components/Label/Label'
-	import StackLayout from '../../../src/layouts/StackLayout'
+import TextArea from '../../../src/core/components/TextArea/TextArea';
+import TextField from '../../../src/core/components/TextField/TextField';
+import StackLayout from '../../../src/layouts/StackLayout';
+import VxpCheckbox from '../../../src/components/VxpCheckbox';
+import VxpLabel from '../../../src/components/VxpLabel';
+import VxpTextField from '../../../src/components/VxpTextField';
 
-	export default {
-		name: "TextAreaDoc",
-		components: {
-      TextArea,
-      TextField,
-			Label,
-			StackLayout
-		},
-		data() {
-			return {
-				text: '',
-				hint: '',
-				editable: true,
-				maxLength: "15",
-				returnKeyType: 'done',
-				autocorrect: false,
-				eventOutput: ''
-			}
+export default {
+  name: 'TextAreaDoc',
+  components: {
+    VxpLabel,
+    VxpCheckbox,
+    TextArea,
+    TextField,
+    StackLayout,
+    VxpTextField,
+  },
+  data() {
+    return {
+      text: '',
+      hint: '',
+      editable: true,
+      maxLength: '15',
+      returnKeyType: 'done',
+      autocorrect: false,
+      eventOutput: '',
+      disabled: false,
+    };
+  },
+  computed: {
+    maxLengthNumeric() {
+      return this.maxLength ? Number(this.maxLength) : null;
     },
-    computed: {
-			maxLengthNumeric() {
-				return this.maxLength ?
-					Number(this.maxLength) : null;
-			}
-		},
-	};
+  },
+};
 </script>
 
 <style>
-	.textView-wrapper * {
-		margin: 3px;
-	}
+.textView-wrapper * {
+  margin: 3px;
+}
 </style>
--- docs/components/textarea.md
+++ docs/components/textarea.md
@@ -8,7 +8,6 @@ This is an overview of the most common usage of `<TextArea>`.
 
 `TextArea` provides two-way data binding using v-model.
 
-
 ## Live Demo
 
 <DocExampleBox codeBox="https://codesandbox.io/s/w61lryop88?module=%2Fsrc%2FApp.vue">
@@ -21,25 +20,25 @@ This is an overview of the most common usage of `<TextArea>`.
 ## Example Usages
 
 ```html
-  <TextArea v-model="textViewInput" :editable=true />
+<textarea v-model="textViewInput" :editable="true" />
 ```
 
 ## Props
 
-| Name | Type | Default | Web | Mobile | Description |
-| --- | --- | --- | --- | --- | --- |
-| text | String | | ✔ | ✔ | Gets or sets the value of the component. |
-| hint | String | | ✔ | ✔ | Gets or sets the placeholder text when the component is editable. |
-| editable | Boolean | `true` | ✔ | ✔ | When true, indicates that the user can edit the contents of the container. |
-| maxLength | Number | unlimited | ✔ | ✔ | Sets the maximum number of characters that can be entered in the container. |
-| returnKeyType | `done` , `next` , `go` , `search` , `send` | | ✘ | ✔ | Gets or sets the label of the return key. |
-| autocorrect | Boolean | | ✔ | ✔ | Enables or disables autocorrect. |
+| Name          | Type                                       | Default   | Web | Mobile | Description                                                                 |
+| ------------- | ------------------------------------------ | --------- | --- | ------ | --------------------------------------------------------------------------- |
+| text          | String                                     |           | ✔   | ✔      | Gets or sets the value of the component.                                    |
+| hint          | String                                     |           | ✔   | ✔      | Gets or sets the placeholder text when the component is editable.           |
+| editable      | Boolean                                    | `true`    | ✔   | ✔      | When true, indicates that the user can edit the contents of the container.  |
+| maxLength     | Number                                     | unlimited | ✔   | ✔      | Sets the maximum number of characters that can be entered in the container. |
+| returnKeyType | `done` , `next` , `go` , `search` , `send` | `done`    | ✘   | ✔      | Gets or sets the label of the return key.                                   |
+| autocorrect   | Boolean                                    | `false`   | ✔   | ✔      | Enables or disables autocorrect.                                            |
 
 ## Events
 
-| Name | Web | Mobile | Description |
-| --- | --- | --- | --- |
-| textChange | ✔ | ✔ | Emitted when the text changes. |
-| returnPress | ✔ | ✔ | Emitted when the return key is pressed. |
-| focus | ✔ | ✔ | Emitted when the container is in focus. |
-| blur | ✔ | ✔ | Emitted when the container loses focus. |
+| Name        | Web | Mobile | Description                             |
+| ----------- | --- | ------ | --------------------------------------- |
+| textChange  | ✔   | ✔      | Emitted when the text changes.          |
+| returnPress | ✔   | ✔      | Emitted when the return key is pressed. |
+| focus       | ✔   | ✔      | Emitted when the container is in focus. |
+| blur        | ✔   | ✔      | Emitted when the container loses focus. |
