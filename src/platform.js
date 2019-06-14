6509fdb02a1845f0889c2505b96262d3dd0b50c1
Metin Kapı <metin.kapi94@gmail.com>
2019-06-14 18:02:36 +0300

    add navigator trick to avoid error

--- src/platform.js
+++ src/platform.js
@@ -4,15 +4,17 @@ function onGridChange(callBack) {
   platformUtils.onGridChange(window, platform.platform, platform.deviceType, platform.screenWidth, callBack);
 }
 
+const _navigator = typeof navigator !== 'undefined' ? navigator : undefined;
+
 const platform = {
   deviceType: 'browser',
   language: 'en',
-  os: platformUtils.getOsName(navigator),
+  os: platformUtils.getOsName(_navigator),
   osVersion: null,
   sdkVersion: null,
   platform: 'web',
-  browser: platformUtils.getBrowser(navigator).name,
-  browserVersion: platformUtils.getBrowser(navigator).version,
+  browser: platformUtils.getBrowser(_navigator).name,
+  browserVersion: platformUtils.getBrowser(_navigator).version,
   screenWidth: platformUtils.getScreenWidth,
   screenHeight: platformUtils.getScreenHeight,
   onGridChange: onGridChange,
